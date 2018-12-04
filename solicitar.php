a4c7b9dbf99c6c16d0ac1a802654f9af1f79ba3f
arkasvzla <Cnx4qsirrA>
2018-12-04 16:45:40 -0400

    confirmat

--- solicitar.php
+++ solicitar.php
@@ -1,9 +1,9 @@
 <?php
 
-$enviado = '';
 $errores = '';
+$enviado = '';
 
-if isset($_POST(['submit'])) {
+if (isset($_POST['submit'])) {
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $cedula = $_POST['cedula'];
@@ -14,61 +14,63 @@ if isset($_POST(['submit'])) {
     $archivo = $_POST['archivo'];
 
     if (!empty($nombre)) {
-        $nombre = trim(nombre);
+        $nombre = trim($nombre);
         $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
     } else {
-        $errores .= 'Por favor ingresar un nombre';
+        $errores .= 'Por favor ingresar un nombre <br />';
     }
 
     if (!empty($apellido)) {
         $apellido = trim($apellido);
         $apellido = filter_var($apellido, FILTER_SANITIZE_STRING);
     } else {
-        $errores .= 'Por favor ingresar un apellido';
+        $errores .= 'Por favor ingresar un apellido <br />';
     }
 
     if (!empty($cedula)) {
         $cedula = trim($cedula);
         $cedula = filter_var($cedula, FILTER_SANITIZE_STRING);
     } else {
-        $errores .= 'Por favor ingresar la cedula';
+        $errores .= 'Por favor ingresar la cedula <br />';
     }
 
     if (!empty($telefono)) {
         $telefono = trim($telefono);
         $telefono = filter_var($telefono, FILTER_SANITIZE_STRING);
     } else {
-        $errores .= 'Por favor ingresar un telefono';
+        $errores .= 'Por favor ingresar un telefono <br />';
     }
 
     if (!empty($correo)) {
         $correo = trim($correo);
         $correo = filter_var($correo, FILTER_SANITIZE_STRING);
     } else {
-        $errores .= ' Por favor ingresar un correo';
+        $errores .= ' Por favor ingresar un correo <br />';
     }
 
     if (!empty($pais)) {
         $pais = trim($pais);
         $pais = filter_var($pais, FILTER_SANITIZE_STRING);
     } else {
-        $errores .= 'Por favor ingresar el pais';
+        $errores .= 'Por favor ingresar el pais <br />';
     }
 
     if (!empty($servicio)) {
         $servicio = trim($servicio);
         $servicio = filter_var($servicio, FILTER_SANITIZE_STRING);
     } else {
-        $errores .= 'Por favor seleccionar el servicio';
+        $errores .= 'Por favor seleccionar el servicio <br />';
     }
     
     if (!empty($archivo)) {
         $archivo = trim($archivo);
         $archivo = filter_var($archivo, FILTER_SANITIZE_STRING);
+    } else {
+        $errores .= 'Por favor ingrese un archivo';
     }
 
     if (!$errores) {
-        $enviar_a 'jdavidr155@gmail.com';
+        $enviar_a = 'jdavidr155@gmail.com';
         $asunto = 'Correo enviando desde www.tudiligenciaya.com.ve';
         $mensaje_preparado = "De: $nombre \n";
         $mensaje_preparado .= "Apellido: $apellido \n";
@@ -77,13 +79,10 @@ if isset($_POST(['submit'])) {
         $mensaje_preparado .= "Correo: $correo \n";
         $mensaje_preparado .= "Pais: $pais \n";
         $mensaje_preparado .= "Servicio: $servicio \n";
-        $mensaje_preparado .= "Documento: $archivo \n";
+        $mensaje_preparado .= "$archivo \n";
         mail($enviar_a, $asunto, $mensaje_preparado);
+        $enviado = 'true';
     }
 }
-
-include 'index.php';
-include 'la_empresa.php';
-include 'servicios.php';
-include 'contacto.php';
+include 'solicitar.view.php';
 ?>
---
+++ solicitar.view.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Solicitar</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/materialize.min.css">
+</head>
+<body>
+<div class="navbar-fixed">
+    <nav>
+        <div class="encabezado nav-wrapper">
+            <div class="container">
+                <a href="index.php" class="brand-logo">Tu Diligencia YA</a>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
+                    <li><a href="la_empresa.php">La Empresa</a></li>
+                    <li><a href="servicios.php">Servicio</a></li>
+                    <li><a href="contacto.php">Contacto</a></li>
+                    <li><a class="btn white black-text waves-effect waves-teal" href="solicitar.php">Solicitar</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>   
+</div>
+
+<ul class="sidenav" id="mobile-demo">
+    <li><a href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
+    <li><a href="la_empresa.php">La Empresa</a></li>
+    <li><a href="servicios.php">Servicios</a></li>
+    <li><a href="contacto.php">Contacto</a></li>
+    <li><a href="solicitar.php" class="btn white black-text waves-effect waves-teal">Solicitar</a></li>
+</ul>
+<br><br><br>
+<div class="container">
+    <center><h4>Servicio de Solicitud</h4></center>
+
+    <div class="row">
+        <div class="col s12">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                <div class="row">                                            
+                    <div class="input-field col s12 m6 l6">
+                        <i class="material-icons prefix">account_box</i>
+                        <input id="nombre" name="nombre" type="text" class="validate">
+                        <label for="nombre">Nombre</label>
+                    </div>
+                    <div class="input-field col s12 m6 l6">
+                        <i class="material-icons prefix">account_box</i>
+                        <input id="apellido" name="apellido" type="text" class="validate">
+                        <label for="apellido">Apellido</label>
+                    </div>
+                    <div class="input-field col s12 m6 l6">
+                        <i class="material-icons prefix">accessibility</i>
+                        <input id="cedula" name="cedula" type="text" class="validate">
+                        <label for="cedula">Cedula</label>
+                    </div>
+                    <div class="input-field col s12 m6 l6">
+                        <i class="material-icons prefix">phone</i>
+                        <input id="telefono" name="telefono" type="text" class="validate">
+                        <label for="telefono">Telefono</label>
+                    </div>
+                    <div class="input-field col s12 m6 l6">
+                        <i class="material-icons prefix">contact_mail</i>
+                        <input id="correo" name="correo" type="email" class="validate">
+                        <label for="correo">Correo</label>
+                    </div>
+                    <div class="input-field col s12 m6 l6">
+                        <i class="material-icons prefix">edit_location</i>
+                        <input id="pais" name="pais" type="text" class="validate">
+                        <label for="pais">Pais</label>
+                    </div>                                                                                      
+                </div>
+                <div class="input-field col s6">
+                    <select name="servicio">
+                    <option selected>Seleccionar Servicio</option>
+                    <option value="1">Apostillado de documento</option>
+                    <option value="2">Antecedentes penales</option>
+                    <option value="3">Gestiones Gubernamentales</option>
+                    <option value="4">Tramites Civiles</option>
+                    <option value="5">Tramites Bancarios</option>
+                    <option value="6">Pago de deudas de servicios Publicos</option>
+                    <option value="7">Supervision de Viviendas</option>
+                    </select>                                                
+                </div>
+                                
+                <div class="file-field input-field col s6">
+                    <div class="btn btn-large">
+                        <span>File</span>
+                        <input name="archivo" type="file" multiple>
+                    </div>
+                    <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text" placeholder="Adjuntar Documento">
+                    </div>
+                </div>
+                
+                <div class="col s6">
+                    <input type="submit" name="submit"  value="enviar">
+                </div>
+                               
+            </form>                            
+        </div>
+    </div>
+            <?php if(!empty($errores)): ?>
+                    <div class="card-panel white-text red">
+                        <?php echo $errores; ?>
+                    </div>
+                <?php elseif ($enviado): ?>
+                    <div class="card-panel green">
+                        <p>Su solicitud ha sido enviada correctamente, estaremos en contacto con usted</p>
+                    </div>
+                <?php endif ?> 
+</div>
+                
+
+
+    <!-- JavaScript -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/sitio.js"></script>
+    <script>
+        // barra de navegacion
+        $(document).ready(function(){
+            $('.sidenav').sidenav();
+        });
+
+        // slider
+        $(document).ready(function(){
+            $('.slider').slider({
+                indicators: false
+            });
+        });       
+     
+        
+        // boton desplegable
+        document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.fixed-action-btn');
+        var instances = M.FloatingActionButton.init(elems, {
+        direction: 'top',
+        hoverEnabled: false
+        });
+        
+        // tooltipeds
+        $(document).ready(function(){
+             $('.tooltipped').tooltip();
+        });
+
+         // Modals
+        $(document).ready(function(){
+             $('.modal').modal();
+        });
+       
+        //select
+        $(document).ready(function(){
+            $('select').formSelect();
+        });
+
+        $(document).ready(function() {
+             $('input#input_text, textarea#textarea2').characterCounter();
+        });
+    });
+    </script>
+</body>
+</html>
